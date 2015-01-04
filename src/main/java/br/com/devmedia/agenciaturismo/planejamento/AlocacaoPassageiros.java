999359b07a7a2b8aed27ec117a19d36987bd9414
marcelocenerine <marcelocenerine@gmail.com>
2015-01-04 15:41:07 -0200

    implementa classe planning solution

---
+++ src/main/java/br/com/devmedia/agenciaturismo/planejamento/AlocacaoPassageiros.java
@@ -0,0 +1,57 @@
+package br.com.devmedia.agenciaturismo.planejamento;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.Solution;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.hardmediumsoft.HardMediumSoftScore;
+
+import br.com.devmedia.agenciaturismo.dominio.Grupo;
+import br.com.devmedia.agenciaturismo.dominio.Veiculo;
+
+@PlanningSolution
+public class AlocacaoPassageiros implements Solution<HardMediumSoftScore> {
+    
+    private List<Grupo> grupos;
+    private List<Veiculo> veiculos;
+    private HardMediumSoftScore score;
+
+    AlocacaoPassageiros() {
+    }
+
+    public AlocacaoPassageiros(List<Grupo> grupos, List<Veiculo> veiculos) {
+        this.grupos = grupos;
+        this.veiculos = veiculos;
+    }
+    
+    @Override
+    public HardMediumSoftScore getScore() {
+        return score;
+    }
+
+    @Override
+    public void setScore(HardMediumSoftScore score) {
+        this.score = score;
+    }
+
+    @ValueRangeProvider(id = "veiculos")
+    public List<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+    
+    @PlanningEntityCollectionProperty
+    public List<Grupo> getGrupos() {
+        return grupos;
+    }
+
+    @Override
+    public Collection<? extends Object> getProblemFacts() {
+        List<Object> facts = new ArrayList<>();
+        facts.addAll(veiculos);
+        return facts;
+    }
+}
